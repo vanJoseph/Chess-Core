6295aad4ca3c4189e77703bf78a62e50335f7c7c
JourniiDev <JourniiFoods@gmail.com>
2020-10-19 12:14:31 -0400

    added a way to get the name of the Piece

--- src/com/wildercoding/chess/PieceType.kt
+++ src/com/wildercoding/chess/PieceType.kt
@@ -1,9 +1,38 @@
 package wildercoding.chess
-enum class PieceType {
-    PAWN,
-    KNIGHT,
-    BISHOP,
-    ROOK,
-    KING,
-    QUEEN
+enum class PieceType: Nameable  {
+    PAWN {
+        override fun getPieceName(): String {
+            return "Pawn"
+        }
+    },
+    KNIGHT {
+        override fun getPieceName(): String {
+            return "Knight"
+        }
+    },
+    BISHOP {
+        override fun getPieceName(): String {
+            return "Bishop"
+        }
+    },
+    ROOK {
+        override fun getPieceName(): String {
+            return "Rook"
+        }
+    },
+    KING {
+        override fun getPieceName(): String {
+            return "King"
+        }
+    },
+    QUEEN {
+        override fun getPieceName(): String {
+            return "Queen"
+        }
+    }
+
+}
+
+interface Nameable {
+    fun getPieceName():String
 }
