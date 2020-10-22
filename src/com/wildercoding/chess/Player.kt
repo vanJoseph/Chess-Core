e9db87504d0c10f1e17bbd3b486307c1581eeb6b
vanJoseph <donovan.wilder@gmail.com>
2020-10-21 21:26:28 -0400

    added the nameable interface

--- src/com/wildercoding/chess/Player.kt
+++ src/com/wildercoding/chess/Player.kt
@@ -1,6 +1,13 @@
 package wildercoding.chess
 
-enum class Player {
-    WHITE,
-    BLACK
-}
+enum class Player:Nameable {
+    WHITE {
+        override fun toName(): String {
+            return "White"
+        }
+    },
+    BLACK;
+
+    override fun toName(): String {
+        return "Black"
+    }}
