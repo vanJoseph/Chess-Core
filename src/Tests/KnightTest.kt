3f72e52182a6f065e73eb2de0d49a0e893761e70
donovan <donovan.wilder@gmail.com>
2020-10-26 17:41:26 -0400

    testing the Knights move

---
+++ src/Tests/KnightTest.kt
@@ -0,0 +1,28 @@
+import org.junit.Test
+import wildercoding.chess.Board
+import wildercoding.chess.Coord
+import wildercoding.chess.Knight
+import wildercoding.chess.Player
+import kotlin.test.assertTrue
+
+class KnightTest {
+
+
+    @Test
+    fun Should_MoveLegal_When_PossibleMoves() {
+        val board = Board()
+        val knight=Knight(Player.WHITE)
+        board.add(Knight(Player.WHITE), Coord(0, 0))
+
+        // legal moves
+        val move1=Coord(2,1)
+        val move2=Coord(1,2)
+        assertTrue(knight.verifyMove(move1))
+
+        printBoard(board)
+    }
+    
+    fun printBoard(board: Board) {
+        println(board.toString())
+    }
+}
