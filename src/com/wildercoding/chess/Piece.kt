6d01e227273f1d3b36598f7e42d4334cb4dd570f
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-09 21:57:18 -0500

    Added a method that filter a list of Coords to those that are in the game board

--- src/com/wildercoding/chess/Piece.kt
+++ src/com/wildercoding/chess/Piece.kt
@@ -4,5 +4,16 @@ abstract class Piece(var piece: PieceType,val color:Player) {
     var location = Coord(-1,-1)
     abstract fun verifyMove(coord: Coord): Boolean
 
-
+companion object{
+    fun filterOutOfBoardPositions(moves: List<Coord>): List<Coord> {
+        var filteredMoves= arrayListOf<Coord>()
+        for (move in moves) {
+            if(move.file>=0||move.file<=7 &&
+                    move.rank>=0 ||move.rank<=7){
+                filteredMoves.add(move)
+            }
+        }
+        return filteredMoves
+    }
+}
 }
