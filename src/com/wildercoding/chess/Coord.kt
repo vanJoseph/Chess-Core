138c312503468e1ac4e66ea5fb9b50a1f059206d
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-09 21:58:03 -0500

    turned the paramaters to var

--- src/com/wildercoding/chess/Coord.kt
+++ src/com/wildercoding/chess/Coord.kt
@@ -1,12 +1,6 @@
 package wildercoding.chess
 
-data class Coord(val file: Int, val rank: Int) {
-//    override fun equals(other: Any?): Boolean {
-//        val otherCoord= other as Coord
-//        if((this.file==otherCoord.file)&&
-//                this.rank==otherCoord.rank)
-//            return true
-//        else
-//            return false
-//    }
+data class Coord(var file: Int, var rank: Int) {
+
+
 }
