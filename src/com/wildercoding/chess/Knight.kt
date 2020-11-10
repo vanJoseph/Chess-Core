1a3bd881e074db78e09a50f3d4ea89d97be62725
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-09 22:25:23 -0500

    added verify move implementation

--- src/com/wildercoding/chess/Knight.kt
+++ src/com/wildercoding/chess/Knight.kt
@@ -2,7 +2,51 @@ package wildercoding.chess
 
 class Knight(color:Player): Piece(PieceType.KNIGHT,color) {
     public override fun verifyMove(coord: Coord): Boolean {
+        val filterdMoves = Piece.filterOutOfBoardPositions(generatMoves())
+        for (move in filterdMoves) {
+            if(move == coord)
+                return true
+        }
+        return false
+    }
 
+    fun generatMoves(): List<Coord> {
+        val moves= arrayListOf<Coord>()
+        var position = location.copy()
+
+        position.rank+=2
+        position.file+=1
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank+=2
+        position.file-=1
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank-=2
+        position.file+=1
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank-=2
+        position.file-=1
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank+=1
+        position.file+=2
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank+=1
+        position.file-=2
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank-=1
+        position.file+=2
+        moves.add(position.copy())
+        position = location.copy()
+        position.rank-=1
+        position.file-=2
+        moves.add(position.copy())
+
+        return moves
     }
 
 }
