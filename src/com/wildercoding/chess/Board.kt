b7f7c3247767e92acf61ab1ef47128f01aadfa58
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-10 21:59:57 -0500

    Working on displaying the board correctly in toString() method

--- src/com/wildercoding/chess/Board.kt
+++ src/com/wildercoding/chess/Board.kt
@@ -1,10 +1,7 @@
 package wildercoding.chess
 
 class Board {
-
-    private var squareInit=  { _:Int ->Square(Coord(0, 0), null)}
-
-    private var board=Array(8){Array<Square>(8,squareInit) }
+    val board=Array(8){rank->Array(8){file->Square(file,rank)}}
 
     fun initNewGame(){
         addPawns()
@@ -14,57 +11,66 @@ class Board {
         addKings()
         addQueens()
     }
-    fun add(piece: Piece, coord: Coord) {
-        board[coord.rank][coord.file].piece=piece
+    fun addPiece(piece: Piece?, coord: Coord) {
+        addPiece(piece,coord.file,coord.rank)
+    }
+    fun addPiece(piece: Piece?, file: Int, rank: Int) {
+        board!![rank][file].piece = piece
+    }
+    fun clearboard(){
+        for (y in 7 downTo 0) {
+            for (x in 0..7) {
+                addPiece(null,Coord(x,y))
+            }
+        }
     }
 
     private fun addPawns() {
         for (y in 0..7){
-            board[1][y].piece=Pawn(Player.WHITE)
-            board[6][y].piece=Pawn(Player.BLACK)
+            board!![1][y].piece=Pawn(Player.WHITE)
+            board!![6][y].piece=Pawn(Player.BLACK)
         }
     }
     private fun addRooks() {
-        board[0][0].piece = Rook(Player.WHITE)
-        board[0][7].piece = Rook(Player.WHITE)
+        board!![0][0].piece = Rook(Player.WHITE)
+        board!![0][7].piece = Rook(Player.WHITE)
 
-        board[7][0].piece = Rook(Player.BLACK)
-        board[7][7].piece = Rook(Player.BLACK)
+        board!![7][0].piece = Rook(Player.BLACK)
+        board!![7][7].piece = Rook(Player.BLACK)
     }
     private fun addKnights() {
-        board[0][1].piece = Knight(Player.WHITE)
-        board[0][6].piece = Knight(Player.WHITE)
+        board!![0][1].piece = Knight(Player.WHITE)
+        board!![0][6].piece = Knight(Player.WHITE)
 
-        board[7][1].piece = Knight(Player.BLACK)
-        board[7][6].piece = Knight(Player.BLACK)
+        board!![7][1].piece = Knight(Player.BLACK)
+        board!![7][6].piece = Knight(Player.BLACK)
     }
     private fun addBishops() {
-        board[0][2].piece = Bishop(Player.WHITE)
-        board[0][5].piece = Bishop(Player.WHITE)
+        board!![0][2].piece = Bishop(Player.WHITE)
+        board!![0][5].piece = Bishop(Player.WHITE)
 
-        board[7][2].piece = Bishop(Player.BLACK)
-        board[7][5].piece = Bishop(Player.BLACK)
+        board!![7][2].piece = Bishop(Player.BLACK)
+        board!![7][5].piece = Bishop(Player.BLACK)
     }
     private fun addQueens() {
-        board[0][3].piece = Queen(Player.WHITE)
+        board!![0][3].piece = Queen(Player.WHITE)
 
-        board[7][3].piece = Queen(Player.BLACK)
+        board!![7][3].piece = Queen(Player.BLACK)
     }
     private fun addKings() {
-        board[0][4].piece = King(Player.WHITE)
+        board!![0][4].piece = King(Player.WHITE)
 
-        board[7][4].piece = King(Player.BLACK)
+        board!![7][4].piece = King(Player.BLACK)
+    }
+    fun getPiece(file: Int, rank: Int): Piece? {
+        return board!![rank][file].piece
     }
     override fun toString(): String {
-         var boardString:String= String()
+         var boardString= String()
         for (rank in 7 downTo 0) {
             for (file in 0..7) {
-                var pieceType=String()
-                if(board[rank][file].piece!=null) {
-                    pieceType= board[rank][file].piece?.piece.toString()+"(${board[rank][file].piece!!.color.toName()[0]})"
-                }else{
-                    pieceType="None"
-                }
+                val piece:Piece?=getPiece(file,rank)
+                var pieceType=piece?.type ?:"None"
                 boardString+="$pieceType:($file,$rank)\t"
             }
             boardString+="\n"
