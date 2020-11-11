aa36ef5c220ca09ab713d00b28f7419c772015fc
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-10 21:56:47 -0500

    changed the field name piece to type

--- src/com/wildercoding/chess/Piece.kt
+++ src/com/wildercoding/chess/Piece.kt
@@ -1,6 +1,6 @@
 package wildercoding.chess
 
-abstract class Piece(var piece: PieceType,val color:Player) {
+abstract class Piece(var type: PieceType, val color:Player) {
     var location = Coord(-1,-1)
     abstract fun verifyMove(coord: Coord): Boolean
 
