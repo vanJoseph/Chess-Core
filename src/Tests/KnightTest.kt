e4b60f80781601d90ebc1f0912114cf6f797f0f1
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-11 21:18:24 -0500

    Test the knight and its movements

--- src/Tests/KnightTest.kt
+++ src/Tests/KnightTest.kt
@@ -1,4 +1,4 @@
-
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import wildercoding.chess.Board
 import wildercoding.chess.Coord
@@ -6,24 +6,38 @@ import wildercoding.chess.Knight
 import wildercoding.chess.Player
 import kotlin.test.assertTrue
 
-class KnightTest {
+class KnightTest: PieceTest {
 
 
     @Test
-    fun Should_MoveLegal_When_PossibleMoves() {
+    override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
         val board = Board()
-        val knight=Knight(Player.WHITE)
+        val knight = Knight(Player.WHITE)
         board.addPiece(knight, Coord(0, 0))
         printBoard(board)
 
         // legal moves
-        val move1=Coord(2,1)
-        val move2=Coord(1,2)
-        assertTrue(knight.verifyMove(move1))
-
-        printBoard(board)
+        val legalMove1 = Coord(2, 1)
+        val legalMove2 = Coord(1, 2)
+        assertTrue(knight.verifyMove(legalMove1))
+        assertTrue(knight.verifyMove(legalMove2))
     }
-    
+
+
+    @Test
+    override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
+        val board = Board()
+        val knight = Knight(Player.WHITE)
+        board.addPiece(knight, Coord(0, 0))
+        printBoard(board)
+
+        // legal moves
+        val illegalMove1 = Coord(0, 0)
+        val illegalMove2 = Coord(7, 7)
+        assertFalse(knight.verifyMove(illegalMove1))
+        assertFalse(knight.verifyMove(illegalMove2))
+    }
+
     fun printBoard(board: Board) {
         println(board.toString())
     }
