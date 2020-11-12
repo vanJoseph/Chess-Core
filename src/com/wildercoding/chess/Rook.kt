cafcb8925b66e17cb8039c0c823fb3d91e0add0e
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-11 22:06:40 -0500

    implemented the Piece.kt abstract methods successfully

--- src/com/wildercoding/chess/Rook.kt
+++ src/com/wildercoding/chess/Rook.kt
@@ -1,8 +1,29 @@
 package wildercoding.chess
 
-class Rook(color: Player) : Piece(PieceType.ROOK,color) {
+class Rook(color: Player) : Piece(PieceType.ROOK, color) {
     override fun verifyMove(coord: Coord): Boolean {
-        TODO("Not yet implemented")
+        if (coord.equals(location))
+            return false
+
+        for (move in generateMovesList()) {
+            if (move.equals(coord))
+                return true
+        }
+        return false
     }
+
+    override fun generateMovesList(): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        // Get the ranked squares
+        for (i in 0..7) {
+            possibleLocations.add(Coord(i, location.rank))
+        }
+        // Get the filed squares
+        for (i in 0..7) {
+            possibleLocations.add(Coord(location.file, i))
+        }
+        return possibleLocations
+    }
+
 
 }
