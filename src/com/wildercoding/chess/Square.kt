1b90508c0fbfeddb3abe4d487674fad11ac5bfca
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-11 20:30:19 -0500

    Fix the bug that kept the field's Piece at null

--- src/com/wildercoding/chess/Square.kt
+++ src/com/wildercoding/chess/Square.kt
@@ -3,12 +3,13 @@ package wildercoding.chess
 class Square (val file:Int, val rank:Int){
     var boardPosition=Coord(file,rank)
     var piece:Piece? = null
-        get() {
-            return field
-        }
         set(value) {
-        if(piece!=null)
-            field!!.location=boardPosition
+        if(value!=null) {
+            field=value
+
+            // Assigns the square boardPosition to the piece location
+            value.location = boardPosition
+        }
     }
 
     override fun toString(): String {
