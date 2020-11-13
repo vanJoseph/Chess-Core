39a8f2caa8645af580de09f6eb9662fe9dfcd62f
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-13 11:28:23 -0500

    Tested and verified the kings movement

--- src/Tests/KingTest.kt
+++ src/Tests/KingTest.kt
@@ -1,9 +1,50 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import wildercoding.chess.*
+
 class KingTest : PieceTest {
+
+    @Test
     override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
-        TODO("Not yet implemented")
+        val board = Board()
+
+        /*
+         Legal moves should include:
+         - Moves in an Open Position
+         - Moves in a corner
+         - Moves along the endges
+         */
+        val legalMoves =arrayOf<Coord>(Coord(0,1), Coord(1,0), Coord(1,1))
+        val startingPosition = Coord(0,0)
+
+        val king = King(Player.WHITE)
+        board.addPiece(king, startingPosition)
+
+        for (move in legalMoves) {
+            assertTrue(king.verifyMove(move))
+        }
     }
 
+    @Test
     override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
-        TODO("Not yet implemented")
+        val board = Board()
+
+        /*
+         Illegal moves should include:
+         - Current Position
+         - Moves outside of the board that are with in its possible moves if the board was extended
+         - Moves that are out side of it legal moves
+         */
+        val illegalMoves =arrayOf<Coord>(Coord(0,0), Coord(-1,-1), Coord(2,2), Coord(0,2), Coord(2,0))
+        val startingPosition = Coord(0,0)
+
+        val king = King(Player.WHITE)
+        board.addPiece(king, startingPosition)
+
+        for (move in illegalMoves) {
+            assertFalse(king.verifyMove(move))
+        }
     }
 }
--- src/com/wildercoding/chess/King.kt
+++ src/com/wildercoding/chess/King.kt
@@ -2,10 +2,32 @@ package wildercoding.chess
 
 class King(color:Player):Piece(PieceType.KING,color) {
     override fun verifyMove(coord: Coord): Boolean {
-        TODO("Not yet implemented")
+        val filterdMoves = generateMovesList()
+        for (move in filterdMoves) {
+            if(move == coord)
+                return true
+        }
+        return false
     }
 
     override fun generateMovesList(): List<Coord> {
-        TODO("Not yet implemented")
+        val possibleMoves = arrayListOf<Coord?>()
+
+        possibleMoves.add(Coord(location.file+1, location.rank))
+        possibleMoves.add(Coord(location.file+1, location.rank-1))
+        possibleMoves.add(Coord(location.file+1, location.rank+1))
+
+        possibleMoves.add(Coord(location.file-1, location.rank))
+        possibleMoves.add(Coord(location.file-1, location.rank-1))
+        possibleMoves.add(Coord(location.file-1, location.rank+1))
+
+        possibleMoves.add(Coord(location.file, location.rank+1))
+        possibleMoves.add(Coord(location.file, location.rank-1))
+
+        for ((index,move) in possibleMoves.withIndex()) {
+            if(!Coord.validateCoord(move!!))
+                possibleMoves[index]=null
+        }
+        return possibleMoves.filterNotNull()
     }
 }
