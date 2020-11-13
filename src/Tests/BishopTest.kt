5cc331ccdee25e6d915f9f9e1861285792def08a
vanJoseph <donovan.wilder@gmail.com>
2020-11-12 19:30:27 -0500

    the finished bishop with test

---
+++ src/Tests/BishopTest.kt
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import wildercoding.chess.*
+
+class BishopTest: PieceTest() {
+    @Test
+    override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
+        val board = Board()
+
+        val legalMoves=arrayOf<Coord>( Coord(1,1), Coord(5,5), Coord(7,7))
+
+        val startlocation= Coord(0,0)
+        val bishop= Bishop(Player.BLACK)
+        board.addPiece(bishop,startlocation)
+
+        for (move in legalMoves) {
+            assertTrue(bishop.verifyMove(move))
+        }
+    }
+@Test
+    override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
+        val board = Board()
+
+        val ilegalMoves=arrayOf<Coord>(Coord(0,0), Coord(0,1), Coord(1,0), Coord(2,0))
+
+        val startlocation= Coord(0,0)
+        val bishop= Bishop(Player.BLACK)
+        board.addPiece(bishop,startlocation)
+
+        for (move in ilegalMoves) {
+            assertFalse(bishop.verifyMove(move))
+        }
+    }
+}
--- src/com/wildercoding/chess/Bishop.kt
+++ src/com/wildercoding/chess/Bishop.kt
@@ -2,10 +2,37 @@ package wildercoding.chess
 
 class Bishop(color:Player):Piece(PieceType.BISHOP,color) {
     override fun verifyMove(coord: Coord): Boolean {
-        TODO("Not yet implemented")
+        if (location.equals(coord)) {
+            return false
+        }
+        for (move in generateMovesList()) {
+            if (move==coord) {
+                return true
+            }
+        }
+        return false
     }
 
     override fun generateMovesList(): List<Coord> {
-        TODO("Not yet implemented")
+        val moveList= arrayListOf<Coord>()
+
+        for (i in 1..7) {
+            val diagonalNE = Coord(location.file+i,location.rank+i)
+            val diagonalNW = Coord(location.file-i,location.rank+i)
+            val diagonalSE = Coord(location.file+i,location.rank-i)
+            val diagonalSW = Coord(location.file-i,location.rank-i)
+
+            if(Coord.validateCoord(diagonalNE))
+                moveList.add(diagonalNE)
+            if(Coord.validateCoord(diagonalNW))
+                moveList.add(diagonalNW)
+            if(Coord.validateCoord(diagonalSE))
+                moveList.add(diagonalSE)
+            if(Coord.validateCoord(diagonalSW))
+                moveList.add(diagonalSW)
+        }
+        return moveList
     }
+
+
 }
