03544d24e7bd627121a7ced40f4a2937a4e7adb4
vanJoseph <donovan.wilder@gmail.com>
2020-11-12 19:29:12 -0500

    added a validateCoord static method

--- src/com/wildercoding/chess/Coord.kt
+++ src/com/wildercoding/chess/Coord.kt
@@ -5,4 +5,15 @@ data class Coord(var file: Int, var rank: Int) {
     override fun toString(): String {
         return "($file,$rank)"
     }
+
+    companion object {
+        /**
+         * Check to see if the Coord is within the range of the board
+         */
+        fun validateCoord(coord: Coord):Boolean {
+            if (coord.file < 0 || coord.file > 7 || coord.rank < 0 || coord.rank > 7)
+                return false
+            return true
+        }
+    }
 }
