6121b6be2464854a3f032aad1e391445e228eb4c
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-12 22:21:51 -0500

    refectored so that therear are no coords out of boardbounds

--- src/com/wildercoding/chess/Bishop.kt
+++ src/com/wildercoding/chess/Bishop.kt
@@ -1,12 +1,12 @@
 package wildercoding.chess
 
-class Bishop(color:Player):Piece(PieceType.BISHOP,color) {
+class Bishop(color: Player) : Piece(PieceType.BISHOP, color) {
     override fun verifyMove(coord: Coord): Boolean {
         if (location.equals(coord)) {
             return false
         }
         for (move in generateMovesList()) {
-            if (move==coord) {
+            if (move == coord) {
                 return true
             }
         }
@@ -14,22 +14,28 @@ class Bishop(color:Player):Piece(PieceType.BISHOP,color) {
     }
 
     override fun generateMovesList(): List<Coord> {
-        val moveList= arrayListOf<Coord>()
+        val moveList = arrayListOf<Coord>()
 
+        val diagonal= Array<Coord?>(4){_->null}
         for (i in 1..7) {
-            val diagonalNE = Coord(location.file+i,location.rank+i)
-            val diagonalNW = Coord(location.file-i,location.rank+i)
-            val diagonalSE = Coord(location.file+i,location.rank-i)
-            val diagonalSW = Coord(location.file-i,location.rank-i)
+            // NE diagonal
+            diagonal[0] = Coord(location.file + i, location.rank + i)
 
-            if(Coord.validateCoord(diagonalNE))
-                moveList.add(diagonalNE)
-            if(Coord.validateCoord(diagonalNW))
-                moveList.add(diagonalNW)
-            if(Coord.validateCoord(diagonalSE))
-                moveList.add(diagonalSE)
-            if(Coord.validateCoord(diagonalSW))
-                moveList.add(diagonalSW)
+            // NW diagonal
+            diagonal[1] = Coord(location.file - i, location.rank + i)
+
+            // SE diagonal
+            diagonal[2] = Coord(location.file + i, location.rank - i)
+
+            // SW diagonal
+            diagonal[3]= Coord(location.file - i, location.rank - i)
+
+
+
+            for (coord in diagonal) {
+                if (Coord.validateCoord(coord!!))
+                    moveList.add(coord)
+            }
         }
         return moveList
     }
