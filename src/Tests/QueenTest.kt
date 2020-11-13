59f2afaa7554ed6c8536645bdc4ff3b487eead03
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-12 21:02:50 -0500

    Tested and verified the queens movement

---
+++ src/Tests/QueenTest.kt
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import wildercoding.chess.*
+
+class QueenTest : PieceTest {
+
+    @Test
+    override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
+        val board = Board()
+
+        val legalMoves=arrayOf<Coord>(Coord(0,1), Coord(1,1), Coord(1,0), Coord(7,7), Coord(0,7), Coord(7,0))
+
+        val startlocation= Coord(0,0)
+        val queen= Queen(Player.BLACK)
+        board.addPiece(queen,startlocation)
+
+        for (move in legalMoves) {
+            assertTrue(queen.verifyMove(move))
+        }
+    }
+
+    @Test
+    override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
+        val board = Board()
+
+        val illegalMoves=arrayListOf<Coord>()
+        val startlocation= Coord(0,0)
+        // The queen should not be able to do knight moves
+        val knight=Knight(Player.BLACK)
+        knight.location = startlocation
+        illegalMoves.addAll(knight.generateMovesList())
+
+
+        val queen= Queen(Player.BLACK)
+        board.addPiece(queen,startlocation)
+
+        for (move in illegalMoves) {
+            assertFalse(queen.verifyMove(move))
+        }
+    }
+}
--- src/com/wildercoding/chess/Queen.kt
+++ src/com/wildercoding/chess/Queen.kt
@@ -2,10 +2,24 @@ package wildercoding.chess
 
 class Queen(color: Player):Piece(PieceType.QUEEN,color) {
     override fun verifyMove(coord: Coord): Boolean {
-        TODO("Not yet implemented")
+        if (location ==coord )
+            return false
+
+        for (move in generateMovesList()) {
+            if(coord == move)
+                return true
+        }
+        return false
     }
 
     override fun generateMovesList(): List<Coord> {
-        TODO("Not yet implemented")
+        val possibleMoves = arrayListOf<Coord>()
+        val rook = Rook(Player.BLACK)
+        rook.location = location
+        val bishop = Bishop(Player.BLACK)
+        bishop.location=location
+        possibleMoves.addAll(rook.generateMovesList())
+        possibleMoves.addAll(bishop.generateMovesList())
+        return possibleMoves
     }
 }
