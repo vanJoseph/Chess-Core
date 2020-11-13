c3d11e0bc6c6730ed3787fba65ce75c7383a16d2
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-12 22:28:43 -0500

    empty king test

---
+++ src/Tests/KingTest.kt
@@ -0,0 +1,9 @@
+class KingTest : PieceTest {
+    override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
+        TODO("Not yet implemented")
+    }
+
+    override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
+        TODO("Not yet implemented")
+    }
+}
