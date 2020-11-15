50d4609e9452b8862e9170cd0d8b953f216033be
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-14 20:48:10 -0500

    Successfuly Test move and non moves

--- src/Tests/PawnTest.kt
+++ src/Tests/PawnTest.kt
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -33,7 +32,28 @@ class PawnTest:PieceTest {
 
     }
 
+    @Test
     override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
-        TODO("Not yet implemented")
+
+        val board = Board()
+        val startingPosition = Coord(0,0)
+        val pawn = Pawn(Player.WHITE)
+        board.addPiece(pawn, startingPosition)
+        // First Move
+        val illegalLegalFirstMoves =arrayOf<Coord>(Coord(0,0),Coord(0,3), Coord(1,2), Coord(1,0), Coord(1,1))
+        pawn.firstMove= true   // Indicate that it is its first move
+
+        // Test the moves
+        for (move in illegalLegalFirstMoves) {
+            assertFalse(pawn.verifyMove(move))
+        }
+
+        // Second Move
+        val legalSecondMove = Coord(0,2)
+        pawn.firstMove= false   // Indicate that it is not the pawns first move
+
+        // Test the moves
+        assertFalse(pawn.generateMovesList().size>1, "Pawn has too many possible moves")
+        assertFalse(pawn.verifyMove(legalSecondMove), "Pawn isn't making the right non-first move")
     }
 }
