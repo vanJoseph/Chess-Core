7124957a02c8267f751e0c422b873a5220a4884b
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-15 13:14:10 -0500

    added removedPiece() method changed signature of the addPiece() method they are not null safe now

--- src/com/wildercoding/chess/Board.kt
+++ src/com/wildercoding/chess/Board.kt
@@ -1,7 +1,7 @@
 package wildercoding.chess
 
 class Board {
-    val board=Array(8){rank->Array(8){file->Square(file,rank)}}
+    val squares=Array(8){ rank->Array(8){ file->Square(file,rank)}}
 
     fun initNewGame(){
         addPawns()
@@ -11,62 +11,69 @@ class Board {
         addKings()
         addQueens()
     }
-    fun addPiece(piece: Piece?, coord: Coord) {
+    fun addPiece(piece: Piece, coord: Coord) {
         addPiece(piece,coord.file,coord.rank)
     }
-    fun addPiece(piece: Piece?, file: Int, rank: Int) {
-        board[rank][file].piece = piece
+    fun addPiece(piece: Piece, file: Int, rank: Int) {
+
+        squares[rank][file].piece = piece
+    }
+    fun removePiece(target: Coord) {
+        removePiece(target.file,target.rank)
+    }
+    fun removePiece(targetFile:Int, targetRank:Int){
+        squares[targetRank][targetFile].piece = null
     }
     fun clearboard(){
-        for (y in 7 downTo 0) {
-            for (x in 0..7) {
-                addPiece(null,Coord(x,y))
+        for (rank in 7 downTo 0) {
+            for (file in 0..7) {
+                removePiece(file,rank)
             }
         }
     }
 
     private fun addPawns() {
         for (y in 0..7){
-            board!![1][y].piece=Pawn(Player.WHITE)
-            board!![6][y].piece=Pawn(Player.BLACK)
+            squares!![1][y].piece=Pawn(Player.WHITE)
+            squares!![6][y].piece=Pawn(Player.BLACK)
         }
     }
     private fun addRooks() {
-        board!![0][0].piece = Rook(Player.WHITE)
-        board!![0][7].piece = Rook(Player.WHITE)
+        squares!![0][0].piece = Rook(Player.WHITE)
+        squares[0][7].piece = Rook(Player.WHITE)
 
-        board!![7][0].piece = Rook(Player.BLACK)
-        board!![7][7].piece = Rook(Player.BLACK)
+        squares!![7][0].piece = Rook(Player.BLACK)
+        squares!![7][7].piece = Rook(Player.BLACK)
     }
     private fun addKnights() {
-        board!![0][1].piece = Knight(Player.WHITE)
-        board!![0][6].piece = Knight(Player.WHITE)
+        squares!![0][1].piece = Knight(Player.WHITE)
+        squares!![0][6].piece = Knight(Player.WHITE)
 
-        board!![7][1].piece = Knight(Player.BLACK)
-        board!![7][6].piece = Knight(Player.BLACK)
+        squares!![7][1].piece = Knight(Player.BLACK)
+        squares!![7][6].piece = Knight(Player.BLACK)
     }
     private fun addBishops() {
-        board!![0][2].piece = Bishop(Player.WHITE)
-        board!![0][5].piece = Bishop(Player.WHITE)
+        squares!![0][2].piece = Bishop(Player.WHITE)
+        squares!![0][5].piece = Bishop(Player.WHITE)
 
-        board!![7][2].piece = Bishop(Player.BLACK)
-        board!![7][5].piece = Bishop(Player.BLACK)
+        squares!![7][2].piece = Bishop(Player.BLACK)
+        squares!![7][5].piece = Bishop(Player.BLACK)
     }
     private fun addQueens() {
-        board!![0][3].piece = Queen(Player.WHITE)
+        squares!![0][3].piece = Queen(Player.WHITE)
 
-        board!![7][3].piece = Queen(Player.BLACK)
+        squares!![7][3].piece = Queen(Player.BLACK)
     }
     private fun addKings() {
-        board!![0][4].piece = King(Player.WHITE)
+        squares!![0][4].piece = King(Player.WHITE)
 
-        board!![7][4].piece = King(Player.BLACK)
+        squares!![7][4].piece = King(Player.BLACK)
     }
     fun getPiece(coord: Coord):Piece?{
         return getPiece( coord.file,coord.rank)
     }
     fun getPiece(file: Int, rank: Int): Piece? {
-        return board!![rank][file].piece
+        return squares[rank][file].piece
     }
     override fun toString(): String {
          var boardString= String()
