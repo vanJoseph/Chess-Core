60644c0f07a054819df949132811c304595a8858
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-15 14:45:11 -0500

    added a Board as a parameter

--- src/com/wildercoding/chess/GameInfo.kt
+++ src/com/wildercoding/chess/GameInfo.kt
@@ -3,4 +3,4 @@ package wildercoding.chess
 /**
  * Give the current information about the game
  */
-data class GameInfo(val playerTurn:Player)
+data class GameInfo(val playerTurn:Player,val board: Board)
