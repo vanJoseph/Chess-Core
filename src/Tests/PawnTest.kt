d9de9a10124bf59aac18f138f7b3d7a2bce45bf2
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-17 11:13:59 -0500

    Added a test to check to see if the pawn is moving in the right direction according to its color

--- src/Tests/PawnTest.kt
+++ src/Tests/PawnTest.kt
@@ -3,19 +3,44 @@ import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import wildercoding.chess.*
 
-class PawnTest:PieceTest {
+class PawnTest : PieceTest {
 
+
+    @Test
+    fun When_PawnMove_Should_OnlyMoveInOneDirection() {
+
+        // Testing White Pawn direction
+        var board = Board()
+        var startPos = Coord(0, 1)
+        var endPos = Coord(0, 0)
+        var pawn = Pawn(Player.WHITE)
+        board.addPiece(pawn,startPos)
+        var gameManager=GameManager(board,DirectInputMethod(startPos,endPos),ConsoleOutputMethod())
+        assertFalse(gameManager.executeMove().success)
+
+        // Testing black Pawn direction
+        board = Board()
+        startPos = Coord(0, 1)
+        endPos = Coord(0, 0)
+        pawn = Pawn(Player.BLACK)
+        board.addPiece(pawn,startPos)
+        gameManager=GameManager(board,DirectInputMethod(startPos,endPos),ConsoleOutputMethod())
+        gameManager.playerTurn=Player.BLACK
+        assertTrue(gameManager.executeMove().success,"Black pawn can not move in the right direction")
+        gameManager=GameManager(board,DirectInputMethod(startPos,Coord(0,2)),ConsoleOutputMethod())
+        assertTrue(gameManager.executeMove().success, "This move is legal for black")
+    }
 
     @Test
     override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
 
         val board = Board()
-        val startingPosition = Coord(0,0)
+        val startingPosition = Coord(0, 0)
         val pawn = Pawn(Player.WHITE)
         board.addPiece(pawn, startingPosition)
         // First Move
-        val legalFirstMoves =arrayOf<Coord>(Coord(0,1), Coord(0,2))
-        pawn.firstMove= true   // Indicate that it is its first move
+        val legalFirstMoves = arrayOf<Coord>(Coord(0, 1), Coord(0, 2))
+        pawn.firstMove = true   // Indicate that it is its first move
 
         // Test the moves
         for (move in legalFirstMoves) {
@@ -23,11 +48,11 @@ class PawnTest:PieceTest {
         }
 
         // Second Move
-        val legalSecondMove = Coord(0,1)
-        pawn.firstMove= false   // Indicate that it is not the pawns first move
+        val legalSecondMove = Coord(0, 1)
+        pawn.firstMove = false   // Indicate that it is not the pawns first move
 
         // Test the moves
-        assertTrue(pawn.generateMovesList().size==1, "Pawn has too many possible moves")
+        assertTrue(pawn.generateMovesList().size == 1, "Pawn has too many possible moves")
         assertTrue(pawn.verifyMove(legalSecondMove), "Pawn isn't making the right non-first move")
 
     }
@@ -36,12 +61,12 @@ class PawnTest:PieceTest {
     override fun Should_Not_VerifyCorrectMovePattern_When_GivenAMove() {
 
         val board = Board()
-        val startingPosition = Coord(0,0)
+        val startingPosition = Coord(0, 0)
         val pawn = Pawn(Player.WHITE)
         board.addPiece(pawn, startingPosition)
         // First Move
-        val illegalLegalFirstMoves =arrayOf<Coord>(Coord(0,0),Coord(0,3), Coord(1,2), Coord(1,0), Coord(1,1))
-        pawn.firstMove= true   // Indicate that it is its first move
+        val illegalLegalFirstMoves = arrayOf<Coord>(Coord(0, 0), Coord(0, 3), Coord(1, 2), Coord(1, 0), Coord(1, 1))
+        pawn.firstMove = true   // Indicate that it is its first move
 
         // Test the moves
         for (move in illegalLegalFirstMoves) {
@@ -49,11 +74,11 @@ class PawnTest:PieceTest {
         }
 
         // Second Move
-        val legalSecondMove = Coord(0,2)
-        pawn.firstMove= false   // Indicate that it is not the pawns first move
+        val legalSecondMove = Coord(0, 2)
+        pawn.firstMove = false   // Indicate that it is not the pawns first move
 
         // Test the moves
-        assertFalse(pawn.generateMovesList().size>1, "Pawn has too many possible moves")
+        assertFalse(pawn.generateMovesList().size > 1, "Pawn has too many possible moves")
         assertFalse(pawn.verifyMove(legalSecondMove), "Pawn isn't making the right non-first move")
     }
 }
