674e0354c203032b071c858c6f88a187df64cef5
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-17 13:00:24 -0500

    added a no pass test

--- src/Tests/RookTest.kt
+++ src/Tests/RookTest.kt
@@ -1,13 +1,33 @@
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import wildercoding.chess.Board
-import wildercoding.chess.Coord
-import wildercoding.chess.Player
-import wildercoding.chess.Rook
+import wildercoding.chess.*
 
 
 class RookTest : PieceTest {
+
+    @Test
+    fun Should_NotPassPiece_When_PieceIsBlockingPath() {
+        val board = Board()
+        val rook = Rook(Player.WHITE)
+        val startlocation = Coord(5, 5)
+        board.addPiece(rook, startlocation)
+        val passingMoves = arrayOf<Coord>(Coord(0, 5), Coord(7, 5), Coord(5, 0), Coord(5, 7))
+        val blockingPieceLocation = arrayOf<Coord>(Coord(1, 3), Coord(6, 5), Coord(5, 3), Coord(5, 6))
+
+        // Populate the board with Blocking pieces
+        for (location in blockingPieceLocation) {
+            board.addPiece(Pawn(Player.WHITE), location)
+        }
+
+        // Attempt the passing moves
+        for (move in passingMoves) {
+            val gameManager = GameManager(board, DirectInputMethod(startlocation, move), null)
+            //val debug=gameManager.executeMove()
+            assertFalse(gameManager.executeMove().success, "This piece can not pass another")
+        }
+
+    }
 
     @Test
     override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
