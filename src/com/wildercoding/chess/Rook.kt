a5f6c895756fcfecbe85451f52587383d0de0e09
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-17 12:59:45 -0500

    Restructure the rook movement check so that it checks in each direction from its position outwards

--- src/com/wildercoding/chess/Rook.kt
+++ src/com/wildercoding/chess/Rook.kt
@@ -1,16 +1,29 @@
 package wildercoding.chess
 
 class Rook(color: Player) : Piece(PieceType.ROOK, color) {
+    constructor(color: Player, board: Board) : this(color) {
+        this.board = board
+    }
+
+    var board: Board? = null
 
     override fun generateMovesList(): List<Coord> {
         val possibleLocations = arrayListOf<Coord?>()
-        // Get the ranked squares
-        for (i in 0..7) {
-            possibleLocations.add(Coord(i, location.rank))
+        // Get the North Squares
+        for (rank in location.rank..7) {
+            possibleLocations.add(Coord(location.file, rank))
         }
-        // Get the filed squares
-        for (i in 0..7) {
-            possibleLocations.add(Coord(location.file, i))
+        // Get the South Squares
+        for (rank in location.rank downTo 0) {
+            possibleLocations.add(Coord(location.file, rank))
+        }
+        // Get the East Squares
+        for (file in location.file..7) {
+            possibleLocations.add(Coord(file, location.rank))
+        }
+        // Get the West Squares
+        for (file in location.file downTo 0) {
+            possibleLocations.add(Coord(file, location.rank))
         }
         return validateLocation(possibleLocations)
     }
