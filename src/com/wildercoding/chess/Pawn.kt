391f38906320308fd603838269700b14360f9b79
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-17 11:10:03 -0500

    Added unique movements based on the color

--- src/com/wildercoding/chess/Pawn.kt
+++ src/com/wildercoding/chess/Pawn.kt
@@ -1,16 +1,26 @@
 package wildercoding.chess
 
-class Pawn(color:Player) : Piece(PieceType.PAWN,color) {
-    var firstMove=true
+class Pawn(color: Player) : Piece(PieceType.PAWN, color) {
+    var firstMove = true
 
     override fun generateMovesList(): List<Coord> {
-        val possibleMoves= arrayListOf<Coord?>()
+        val possibleMoves = arrayListOf<Coord?>()
         when (firstMove) {
-            true->{
-                possibleMoves.add(Coord(location.file, location.rank+1))
-                possibleMoves.add(Coord(location.file, location.rank+2))
+            true -> {
+                if (color == Player.BLACK) {
+                    possibleMoves.add(Coord(location.file, location.rank - 1))
+                    possibleMoves.add(Coord(location.file, location.rank - 2))
+                } else {
+                    possibleMoves.add(Coord(location.file, location.rank + 1))
+                    possibleMoves.add(Coord(location.file, location.rank + 2))
+                }
             }
-            false-> possibleMoves.add(Coord(location.file, location.rank+1))
+            false -> {
+                if (color == Player.BLACK)
+                    possibleMoves.add(Coord(location.file, location.rank - 1))
+                else
+                    possibleMoves.add(Coord(location.file, location.rank + 1))
+            }
         }
 
         return validateLocation(possibleMoves)
