3dc758567788e8c5c188da6863793b9e0c35fbe4
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-18 11:30:41 -0500

    Tested and Implemented non passing for the Bishop Piece

--- src/Tests/BishopTest.kt
+++ src/Tests/BishopTest.kt
@@ -4,6 +4,27 @@ import org.junit.jupiter.api.Test
 import wildercoding.chess.*
 
 class BishopTest: PieceTest {
+
+
+    @Test
+    fun Should_NotPassPiece_When_PieceIsBlockingPath() {
+        val board = Board()
+        val bishop = Bishop(Player.WHITE,board)
+        val startlocation = Coord(4, 3)
+        board.addPiece(bishop, startlocation)
+        val passingMoves = arrayOf<Coord>(Coord(0, 7), Coord(7, 6), Coord(1, 0), Coord(7, 0))
+        val blockingPieceLocation = arrayOf<Coord>(Coord(2, 1), Coord(2, 5), Coord(6, 5), Coord(7, 0))
+
+        // Populate the board with Blocking pieces
+        for (location in blockingPieceLocation) {
+            board.addPiece(Pawn(Player.WHITE), location)
+        }
+        // Test the passing move in all directions
+        for (move in passingMoves) {
+            assertFalse(bishop.verifyMove(move), "This piece can not pass another")
+        }
+
+    }
     @Test
     override fun Should_VerifyCorrectMovePattern_When_GivenAMove() {
         val board = Board()
@@ -15,7 +36,7 @@ class BishopTest: PieceTest {
         board.addPiece(bishop,startlocation)
 
         for (move in legalMoves) {
-            assertTrue(bishop.verifyMove(move))
+            assertTrue(bishop.verifyMove(move),"legalMove:$move is unreachable from: $startlocation")
         }
     }
 @Test
--- src/com/wildercoding/chess/Bishop.kt
+++ src/com/wildercoding/chess/Bishop.kt
@@ -1,32 +1,87 @@
 package wildercoding.chess
 
 class Bishop(color: Player) : Piece(PieceType.BISHOP, color) {
+    constructor(color: Player, board: Board) : this(color) {
+        this.board = board
+    }
 
+    var board: Board? = null
     override fun generateMovesList(): List<Coord> {
-        val moveList = arrayListOf<Coord>()
+        val possibleLocations = arrayListOf<Coord?>()
+        // If there is a board no passing will be permitted
+        if (board == null) {
 
-        val diagonal= Array<Coord?>(4){_->null}
-        for (i in 1..7) {
-            // NE diagonal
-            diagonal[0] = Coord(location.file + i, location.rank + i)
+            for (i in 1..7) {
+                // Add the NE Squares
+                var nextCoord = Coord(location.file + i, location.rank + i)
+                if (checkInbounds(nextCoord)) {
+                    possibleLocations.add(nextCoord)
+                }
+                // Add the NW Squares
+                nextCoord = Coord(location.file - i, location.rank + i)
+                if (checkInbounds(nextCoord)) {
+                    possibleLocations.add(nextCoord)
 
-            // NW diagonal
-            diagonal[1] = Coord(location.file - i, location.rank + i)
+                }
 
-            // SE diagonal
-            diagonal[2] = Coord(location.file + i, location.rank - i)
+                // Add the SE Squares
+                nextCoord = Coord(location.file + i, location.rank - i)
+                if (checkInbounds(nextCoord)) {
+                    possibleLocations.add(nextCoord)
 
-            // SW diagonal
-            diagonal[3]= Coord(location.file - i, location.rank - i)
+                }
+                // Add the SW Squares
+                nextCoord = Coord(location.file - i, location.rank - i)
+                if (checkInbounds(nextCoord)) {
+                    possibleLocations.add(nextCoord)
 
+                }
+            }
+        } else {
+            // Add the NE Squares
+            for (i in 1..7) {
+                val nextCoord = Coord(location.file + i, location.rank + i)
+                if (checkInbounds(nextCoord) && board!!.getPiece(nextCoord) == null) {
+                    possibleLocations.add(nextCoord)
+                }else
+                    break
+            }
+            // Add the NW Squares
+            for (i in 1..7) {
+                val nextCoord = Coord(location.file - i, location.rank + i)
+                if (checkInbounds(nextCoord) && board!!.getPiece(nextCoord) == null) {
+                    possibleLocations.add(nextCoord)
+                }else
+                    break
+            }
 
-
-            for (coord in diagonal) {
-                if (Coord.validateCoord(coord!!))
-                    moveList.add(coord)
+            // Add the SE Squares
+            for (i in 1..7) {
+                val nextCoord = Coord(location.file + i, location.rank - i)
+                if (checkInbounds(nextCoord) && board!!.getPiece(nextCoord) == null) {
+                    possibleLocations.add(nextCoord)
+                }else
+                    break
+            }
+            // Add the SW Squares
+            for (i in 1..7) {
+                val nextCoord = Coord(location.file - i, location.rank - i)
+                if (checkInbounds(nextCoord) && board!!.getPiece(nextCoord) == null) {
+                    possibleLocations.add(nextCoord)
+                }else
+                    break
             }
         }
-        return moveList
+
+        return validateLocation(possibleLocations)
+    }
+
+    private fun checkInbounds(coord: Coord): Boolean {
+        if (coord.file >= 0 || coord.file <= 7 &&
+                coord.rank >= 0 || coord.rank <= 7)
+            return true
+        else
+            return false
     }
 
 
