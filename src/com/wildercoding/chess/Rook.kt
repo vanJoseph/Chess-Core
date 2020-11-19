29f599a761eed3f97bf0fcf4a4b3ab83529c0f52
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-19 11:31:51 -0500

    added abstract classes from the Piece parent extracted getMove methods from the generateMove method

--- src/com/wildercoding/chess/Rook.kt
+++ src/com/wildercoding/chess/Rook.kt
@@ -14,56 +14,96 @@ class Rook(color: Player) : Piece(PieceType.ROOK, color) {
         // If there is a board no passing will be permitted
         if (board==null) {
             // Get the North Squares
-            for (rank in location.rank..7) {
-                possibleLocations.add(Coord(location.file, rank))
-            }
+            getNorthMoveRange(possibleLocations)
             // Get the South Squares
-            for (rank in location.rank downTo 0) {
-                possibleLocations.add(Coord(location.file, rank))
-            }
+            getSouthMoveRange(possibleLocations)
             // Get the East Squares
-            for (file in location.file..7) {
-                possibleLocations.add(Coord(file, location.rank))
-            }
+            getEastMoveRange(possibleLocations)
             // Get the West Squares
-            for (file in location.file downTo 0) {
-                possibleLocations.add(Coord(file, location.rank))
-            }
+            getWestMoveRange(possibleLocations)
         }else {
             // Get the North Squares
-            for (rank in location.rank..7) {
-                if (board!!.getPiece(location.file,rank)==null) {
-                    possibleLocations.add(Coord(location.file, rank))
-                }else{
-                    break
-                }
-            }
+            getNorthMoves(possibleLocations)
             // Get the South Squares
-            for (rank in location.rank downTo 0) {
-                if (board!!.getPiece(location.file,rank)==null) {
-                    possibleLocations.add(Coord(location.file, rank))
-                }else{
-                    break
-                }
-            }
+            getSouthMoves(possibleLocations)
             // Get the East Squares
-            for (file in location.file..7) {
-                if (board!!.getPiece(file,location.rank)==null) {
-                    possibleLocations.add(Coord(file, location.rank))
-                }else{
-                    break
-                }
-            }
+            getEastMoves(possibleLocations)
             // Get the West Squares
-            for (file in location.file downTo 0) {
-                if (board!!.getPiece(file,location.rank)==null) {
-                    possibleLocations.add(Coord(file, location.rank))
-                }else{
-                    break
-                }
-            }
+            getWestMoves(possibleLocations)
         }
         return validateLocation(possibleLocations)
+    }
+
+    private fun getNorthMoveRange(possibleLocations: ArrayList<Coord?>) {
+        for (rank in location.rank..7) {
+            possibleLocations.add(Coord(location.file, rank))
+        }
+    }
+
+    private fun getSouthMoveRange(possibleLocations: ArrayList<Coord?>) {
+        for (rank in location.rank downTo 0) {
+            possibleLocations.add(Coord(location.file, rank))
+        }
+    }
+
+    private fun getEastMoveRange(possibleLocations: ArrayList<Coord?>) {
+        for (file in location.file..7) {
+            possibleLocations.add(Coord(file, location.rank))
+        }
+    }
+
+    private fun getWestMoveRange(possibleLocations: ArrayList<Coord?>) {
+        for (file in location.file downTo 0) {
+            possibleLocations.add(Coord(file, location.rank))
+        }
+    }
+
+    private fun getNorthMoves(possibleLocations: ArrayList<Coord?>) {
+        for (rank in location.rank..7) {
+            if (board!!.getPiece(location.file, rank) == null) {
+                possibleLocations.add(Coord(location.file, rank))
+            } else {
+                break
+            }
+        }
+    }
+
+    private fun getSouthMoves(possibleLocations: ArrayList<Coord?>) {
+        for (rank in location.rank downTo 0) {
+            if (board!!.getPiece(location.file, rank) == null) {
+                possibleLocations.add(Coord(location.file, rank))
+            } else {
+                break
+            }
+        }
+    }
+
+    private fun getEastMoves(possibleLocations: ArrayList<Coord?>) {
+        for (file in location.file..7) {
+            if (board!!.getPiece(file, location.rank) == null) {
+                possibleLocations.add(Coord(file, location.rank))
+            } else {
+                break
+            }
+        }
+    }
+
+    private fun getWestMoves(possibleLocations: ArrayList<Coord?>) {
+        for (file in location.file downTo 0) {
+            if (board!!.getPiece(file, location.rank) == null) {
+                possibleLocations.add(Coord(file, location.rank))
+            } else {
+                break
+            }
+        }
+    }
+
+    override fun generateTakeList(): List<Coord> {
+
+    }
+
+    override fun verifyTake(coord: Coord): Boolean {
+        TODO("Not yet implemented")
     }
 
 
