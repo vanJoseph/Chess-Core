8ff85a9dffa345bfe08414b03440d27ffa26330d
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-23 09:24:36 -0500

    Turned validateCoord() into a static method Created a static method called getValidatedCoord()

--- src/com/wildercoding/chess/Coord.kt
+++ src/com/wildercoding/chess/Coord.kt
@@ -2,19 +2,32 @@ package wildercoding.chess
 
 data class Coord(var file: Int, var rank: Int) {
     init {
-        if (validateCoord(file,rank)==false)
-            throw InvalidCoordException(file,rank)
+        if (!validateCoord(file, rank))
+            throw InvalidCoordException(file, rank)
     }
 
     override fun toString(): String {
         return "($file,$rank)"
     }
+
+
+    companion object {
         /**
          * Check to see if the Coord is within the range of the board
          */
-        private fun validateCoord(file:Int, rank:Int ):Boolean {
+        fun validateCoord(file: Int, rank: Int): Boolean {
             if (file < 0 || file > 7 || rank < 0 || rank > 7)
                 return false
             return true
         }
+
+        /**
+         *Return a Choord that is in range. Returns Null if not
+         */
+        fun getValidatedCoord(file: Int, rank: Int): Coord? {
+            if(validateCoord(file,rank))
+                return Coord(file,rank)
+            return null
+        }
+    }
 }
