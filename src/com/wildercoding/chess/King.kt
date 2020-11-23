2d333a530e923b20ed125e5f5ab1e6bc544e6a1a
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-23 10:18:04 -0500

    Improved the generateMovesList() method

--- src/com/wildercoding/chess/King.kt
+++ src/com/wildercoding/chess/King.kt
@@ -1,21 +1,32 @@
 package wildercoding.chess
 
-class King(color:Player):Piece(PieceType.KING,color) {
+class King(color:Color):Piece(PieceType.KING,color) {
+    override fun verifyTake(): Boolean {
+        TODO("Not yet implemented")
+    }
 
-    override fun generateMovesList(): List<Coord> {
-        val possibleMoves = arrayListOf<Coord?>()
+    override fun generateMovesList(position:Coord): List<Coord> {
+       val possibleMoves = arrayListOf<Coord?>()
 
-        possibleMoves.add(Coord(location.file+1, location.rank))
-        possibleMoves.add(Coord(location.file+1, location.rank-1))
-        possibleMoves.add(Coord(location.file+1, location.rank+1))
+        possibleMoves.add(Coord.getValidatedCoord(position.file+1, position.rank) ?:null)
+        possibleMoves.add(Coord.getValidatedCoord(position.file+1, position.rank-1) ?:null)
+        possibleMoves.add(Coord.getValidatedCoord(position.file+1, position.rank+1) ?:null)
 
-        possibleMoves.add(Coord(location.file-1, location.rank))
-        possibleMoves.add(Coord(location.file-1, location.rank-1))
-        possibleMoves.add(Coord(location.file-1, location.rank+1))
+        possibleMoves.add(Coord.getValidatedCoord(position.file-1, position.rank) ?:null)
+        possibleMoves.add(Coord.getValidatedCoord(position.file-1, position.rank-1) ?:null)
+        possibleMoves.add(Coord.getValidatedCoord(position.file-1, position.rank+1) ?:null)
 
-        possibleMoves.add(Coord(location.file, location.rank+1))
-        possibleMoves.add(Coord(location.file, location.rank-1))
+        possibleMoves.add(Coord.getValidatedCoord(position.file, position.rank+1) ?:null)
+        possibleMoves.add(Coord.getValidatedCoord(position.file, position.rank-1) ?:null)
 
-        return validateLocation(possibleMoves)
+        return possibleMoves.filterNotNull()
+    }
+
+    override fun generateTakeList(): List<Coord> {
+        TODO("Not yet implemented")
+    }
+
+     fun verifyTake(coord: Coord): Boolean {
+        TODO("Not yet implemented")
     }
 }
