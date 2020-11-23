68c91369a00720f1260528268c930b9649fb5176
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-23 12:36:42 -0500

    Cleaned up the code

--- src/com/wildercoding/chess/Rook.kt
+++ src/com/wildercoding/chess/Rook.kt
@@ -1,109 +1,58 @@
 package wildercoding.chess
 
-class Rook(color: Player) : Piece(PieceType.ROOK, color) {
-    constructor(color: Player, board: Board) : this(color) {
-        this.board = board
+class Rook(color: Color) : Piece(PieceType.ROOK, color) {
+
+    override fun generateMovesList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+
+        possibleLocations.addAll(generateNorthMoveList(coord))
+        possibleLocations.addAll(generateSouthMoveList(coord))
+        possibleLocations.addAll(generateWestMoveList(coord))
+        possibleLocations.addAll(generateEastMoveList(coord))
+
+        return possibleLocations
     }
 
-    var board: Board? = null
-
-    override fun generateMovesList(): List<Coord> {
-
-        val possibleLocations = arrayListOf<Coord?>()
-
-        // If there is a board no passing will be permitted
-        if (board==null) {
-            // Get the North Squares
-            getNorthMoveRange(possibleLocations)
-            // Get the South Squares
-            getSouthMoveRange(possibleLocations)
-            // Get the East Squares
-            getEastMoveRange(possibleLocations)
-            // Get the West Squares
-            getWestMoveRange(possibleLocations)
-        }else {
-            // Get the North Squares
-            getNorthMoves(possibleLocations)
-            // Get the South Squares
-            getSouthMoves(possibleLocations)
-            // Get the East Squares
-            getEastMoves(possibleLocations)
-            // Get the West Squares
-            getWestMoves(possibleLocations)
+    fun generateNorthMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (rank in coord.rank + 1..7) {
+            possibleLocations.add(Coord(coord.file, rank))
         }
-        return validateLocation(possibleLocations)
+        return possibleLocations
     }
 
-    private fun getNorthMoveRange(possibleLocations: ArrayList<Coord?>) {
-        for (rank in location.rank..7) {
-            possibleLocations.add(Coord(location.file, rank))
+    fun generateSouthMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (rank in coord.rank - 1 downTo 0) {
+            possibleLocations.add(Coord(coord.file, rank))
         }
+        return possibleLocations
     }
 
-    private fun getSouthMoveRange(possibleLocations: ArrayList<Coord?>) {
-        for (rank in location.rank downTo 0) {
-            possibleLocations.add(Coord(location.file, rank))
+    fun generateWestMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (file in coord.file - 1 downTo 0) {
+            possibleLocations.add(Coord(file, coord.rank))
         }
+        return possibleLocations
     }
 
-    private fun getEastMoveRange(possibleLocations: ArrayList<Coord?>) {
-        for (file in location.file..7) {
-            possibleLocations.add(Coord(file, location.rank))
+    fun generateEastMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (file in coord.file + 1..7) {
+            possibleLocations.add(Coord(file, coord.rank))
         }
+        return possibleLocations
     }
 
-    private fun getWestMoveRange(possibleLocations: ArrayList<Coord?>) {
-        for (file in location.file downTo 0) {
-            possibleLocations.add(Coord(file, location.rank))
-        }
+    override fun verifyTake(): Boolean {
+        TODO("Not yet implemented")
     }
 
-    private fun getNorthMoves(possibleLocations: ArrayList<Coord?>) {
-        for (rank in location.rank..7) {
-            if (board!!.getPiece(location.file, rank) == null) {
-                possibleLocations.add(Coord(location.file, rank))
-            } else {
-                break
-            }
-        }
-    }
-
-    private fun getSouthMoves(possibleLocations: ArrayList<Coord?>) {
-        for (rank in location.rank downTo 0) {
-            if (board!!.getPiece(location.file, rank) == null) {
-                possibleLocations.add(Coord(location.file, rank))
-            } else {
-                break
-            }
-        }
-    }
-
-    private fun getEastMoves(possibleLocations: ArrayList<Coord?>) {
-        for (file in location.file..7) {
-            if (board!!.getPiece(file, location.rank) == null) {
-                possibleLocations.add(Coord(file, location.rank))
-            } else {
-                break
-            }
-        }
-    }
-
-    private fun getWestMoves(possibleLocations: ArrayList<Coord?>) {
-        for (file in location.file downTo 0) {
-            if (board!!.getPiece(file, location.rank) == null) {
-                possibleLocations.add(Coord(file, location.rank))
-            } else {
-                break
-            }
-        }
-    }
 
     override fun generateTakeList(): List<Coord> {
-
-    }
-
-    override fun verifyTake(coord: Coord): Boolean {
-        TODO("Not yet implemented")
+        val possibleTakes = listOf<Coord>()//getNorthMoves()
+        return possibleTakes
     }
 
 
