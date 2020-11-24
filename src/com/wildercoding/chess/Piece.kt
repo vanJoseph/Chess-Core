2fc5c3b6f4ce839ed4415edb4accd9f9c1334923
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-24 10:54:52 -0500

    Removed unnnecessary methods

--- src/com/wildercoding/chess/Piece.kt
+++ src/com/wildercoding/chess/Piece.kt
@@ -1,40 +1,8 @@
 package wildercoding.chess
 
-import java.lang.RuntimeException
+import java.text.FieldPosition
 
-abstract class Piece(var type: PieceType, val color: Player) {
-    var location = Coord(-1, -1)
-    abstract fun generateMovesList(): List<Coord>
+abstract class Piece(var type: PieceType, val color: Color) {
+    abstract fun generateMovesList(location: Coord): List<Coord>
 
-    fun verifyMove(coord: Coord): Boolean {
-        val filterdMoves = generateMovesList()
-
-        if(coord==location) // The current position is not a valid move
-            return false
-        for (move in filterdMoves) {
-            if (move == coord)
-                return true
-        }
-        return false
-    }
-fun validateLocation(possibleMoves:ArrayList<Coord?>):List<Coord> {
-    for ((index, move) in possibleMoves.withIndex()) {
-        if (!Coord.validateCoord(move!!))
-            possibleMoves[index] = null
-    }
-    return possibleMoves.filterNotNull()
-}
-    companion object {
-
-        fun filterOutOfBoardPositions(moves: List<Coord>): List<Coord> {
-            var filteredMoves = arrayListOf<Coord>()
-            for (move in moves) {
-                if (move.file >= 0 || move.file <= 7 &&
-                        move.rank >= 0 || move.rank <= 7) {
-                    filteredMoves.add(move)
-                }
-            }
-            return filteredMoves
-        }
-    }
 }
