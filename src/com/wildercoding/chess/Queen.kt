1e9349f3428a59dd2d666b0df24f2960ae183a25
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-24 09:20:02 -0500

    reorganized the queen members

--- src/com/wildercoding/chess/Queen.kt
+++ src/com/wildercoding/chess/Queen.kt
@@ -1,26 +1,90 @@
 package wildercoding.chess
 
-class Queen(color: Player) : Piece(PieceType.QUEEN, color) {
-    constructor(color: Player, board: Board?) : this(color) {
-        this.board = board
+class Queen(color: Color) : Piece(PieceType.QUEEN, color) {
+
+    override fun generateMovesList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+
+        possibleLocations.addAll(generateNorthMoveList(coord))
+        possibleLocations.addAll(generateSouthMoveList(coord))
+        possibleLocations.addAll(generateWestMoveList(coord))
+        possibleLocations.addAll(generateEastMoveList(coord))
+        possibleLocations.addAll(generateNeMoveList(coord))
+        possibleLocations.addAll(generateNwMoveList(coord))
+        possibleLocations.addAll(generateSeMoveList(coord))
+        possibleLocations.addAll(generateSwMoveList(coord))
+
+        return possibleLocations
+    }
+    fun generateNorthMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (rank in coord.rank + 1..7) {
+            possibleLocations.add(Coord(coord.file, rank))
+        }
+        return possibleLocations
     }
 
-    var board: Board? = null
-    override fun generateMovesList(): List<Coord> {
-        val possibleMoves = arrayListOf<Coord>()
-        var rook: Rook
-        var bishop: Bishop
-        if (board==null) {
-            rook = Rook(color)
-            bishop = Bishop(color)
-        }else{
-            rook = Rook(color,board!!)
-            bishop = Bishop(color,board!!)
+    fun generateSouthMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (rank in coord.rank - 1 downTo 0) {
+            possibleLocations.add(Coord(coord.file, rank))
         }
-        rook.location = location
-        bishop.location = location
-        possibleMoves.addAll(rook.generateMovesList())
-        possibleMoves.addAll(bishop.generateMovesList())
-        return possibleMoves
+        return possibleLocations
+    }
+    fun generateWestMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (file in coord.file - 1 downTo 0) {
+            possibleLocations.add(Coord(file, coord.rank))
+        }
+        return possibleLocations
+    }
+    fun generateEastMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (file in coord.file + 1..7) {
+            possibleLocations.add(Coord(file, coord.rank))
+        }
+        return possibleLocations
+    }
+    fun generateNeMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (i in 1..7) {
+            val nextCoord = Coord.getValidatedCoord(coord.file + i, coord.rank + i)
+            possibleLocations.add(nextCoord ?:break)
+        }
+        return possibleLocations
+    }
+
+    fun generateSeMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (i in 1..7) {
+            val nextCoord = Coord.getValidatedCoord(coord.file + i, coord.rank- i)
+            possibleLocations.add(nextCoord ?:break)
+        }
+        return possibleLocations
+    }
+
+    fun generateNwMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (i in 1..7) {
+            val nextCoord = Coord.getValidatedCoord(coord.file -i, coord.rank+i)
+            possibleLocations.add(nextCoord ?:break)
+        }
+        return possibleLocations
+    }
+
+    fun generateSwMoveList(coord: Coord): List<Coord> {
+        val possibleLocations = arrayListOf<Coord>()
+        for (i in 1..7) {
+            val nextCoord = Coord.getValidatedCoord(coord.file -i, coord.rank-i)
+            possibleLocations.add(nextCoord ?:break)
+        }
+        return possibleLocations
+    }
+    override fun generateTakeList(): List<Coord> {
+        TODO("Not yet implemented")
+    }
+
+    override fun verifyTake(): Boolean {
+        TODO("Not yet implemented")
     }
 }
