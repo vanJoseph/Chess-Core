768ec3a5b35b728a609698328b80bd60fe36a33d
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-25 12:54:33 -0500

    Reduced Params to the basics

--- src/com/wildercoding/chess/MoveRequest.kt
+++ src/com/wildercoding/chess/MoveRequest.kt
@@ -1,10 +1,3 @@
 package wildercoding.chess
 
-data class MoveRequest(var pieceType: PieceType?,
-                       var fromPos: Coord,
-                       var toPos: Coord,
-                       var moveType: MoveType,
-                       var pieceValidation: Boolean = false,
-                       var moveValidation: Boolean = false,
-                       var executeMove: Boolean = false
-)
+data class MoveRequest(var fromPos: Coord, var toPos: Coord)
