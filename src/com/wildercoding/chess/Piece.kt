c19fc327947297bb59e8f7007061f6920465749e
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-26 12:10:00 -0500

    added comment for verifyMove() method

--- src/com/wildercoding/chess/Piece.kt
+++ src/com/wildercoding/chess/Piece.kt
@@ -4,6 +4,9 @@ import java.text.FieldPosition
 
 abstract class Piece(val type: PieceType, val color: Color) {
     abstract fun generateMovesList(location: Coord): List<Coord>
+    /**
+     * Checks to make sure that there is no pieces between the fromPos and the toPos
+     */
     abstract fun verifyMove(board: Board, fromPos:Coord, toPos:Coord): Boolean
     abstract fun verifyTake(board: Board, fromPos:Coord, toPos: Coord): Boolean
 
