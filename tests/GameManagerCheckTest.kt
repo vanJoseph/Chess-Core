4c94b1bf56dd5eafdb8ee69f84cb13c28c19b8a9
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-11-30 16:51:49 -0500

    Init Commit

---
+++ tests/GameManagerCheckTest.kt
@@ -0,0 +1,81 @@
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+import org.junit.Test
+import wildercoding.chess.*
+
+class GameManagerCheckTest {
+    lateinit var board: Board
+    lateinit var gameManager: GameManager
+    @Before
+    fun setup(){
+        board = Board()
+        gameManager = GameManager(board)
+    }
+
+    fun cycleThruBoardCoords(assertLambda:(coord: Coord)->Unit){
+        for (y in 0..7) {
+            for (x in 0 .. 7){
+                assertLambda(Coord(x,y))
+            }
+        }
+    }
+    @Test
+    fun Should_NotCheck_When_BoardIsEmpty() {
+        val assertLambda = {coord: Coord -> assertThat(gameManager.checkForCheck(coord,Color.BLACK),`is`(false))}
+        cycleThruBoardCoords(assertLambda)
+    }
+
+    @Test
+    fun Should_Check_When_InCheckByPawn() {
+        board.addPiece(Pawn(Color.BLACK),Coord(3,2))
+        val testSquare = Coord(2,1)
+        assertThat(gameManager.checkForCheck(testSquare,Color.BLACK),`is`(true))
+    }
+
+    @Test
+    fun Should_Check_When_InCheckByKing() {
+        val testSquare = Coord(3,3)
+        val kingSquares = arrayOf(
+                Coord(4,4),Coord(2,4),
+                Coord(4,3),Coord(2,3),
+                Coord(4,2),Coord(2,2),
+                Coord(3,4),Coord(3,2))
+
+        for (pos in kingSquares) {
+            board.addPiece(King(Color.WHITE),pos)
+            assertThat(gameManager.checkForCheck(testSquare,Color.WHITE), `is`(true))
+            board.removePiece(pos)
+        }
+    }
+
+    @Test
+    fun Should_Check_When_InCheckByKnight() {
+        val testSquare = Coord(3,3)
+        val knightSquares = arrayOf(
+                Coord(2,5),Coord(4,5),
+                Coord(1,4),Coord(5,4),
+                Coord(1,2),Coord(5,2),
+                Coord(2,1),Coord(4,1))
+
+        for (pos in knightSquares) {
+            board.addPiece(Knight(Color.WHITE),pos)
+            assertThat(gameManager.checkForCheck(testSquare,Color.WHITE), `is`(true))
+            board.removePiece(pos)
+        }
+    }
+
+    @Test
+    fun Should_Check_When_InCheckByRook() {
+        val testSquare = Coord(3,3)
+        val rookSquares = arrayOf(
+                Coord(3,7), Coord(3,0),
+                Coord(0,3), Coord(7,3))
+
+        for (pos in rookSquares) {
+            board.addPiece(Rook(Color.WHITE),pos)
+            assertThat(gameManager.checkForCheck(testSquare,Color.WHITE), `is`(true))
+            board.removePiece(pos)
+        }
+    }
+}
