19dbe2f14fade4fa48ca696c3ad4cf49fd355789
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-12-01 14:09:24 -0500

    implemented input validation

--- src/com/wildercoding/chess/ConsoleInputMethod.kt
+++ src/com/wildercoding/chess/ConsoleInputMethod.kt
@@ -2,35 +2,55 @@ package wildercoding.chess
 
 class ConsoleInputMethod: InputMethod {
     override fun getMove(): MoveRequest {
-        println("Choose piece: ")
-        val fromPos = readLine()!!
+        var fromLoc: String? =null
+        while (fromLoc == null) {
+            fromLoc =getFromLocation()
+            if(!validateInput(fromLoc ?:continue)){
+                fromLoc = null
+            }
+        }
 
-
-        println("Choose move: ")
-        val toPos = readLine()!!
-
-        return parseInput(fromPos,toPos)
+        var toLoc: String? =null
+        while (toLoc == null) {
+            toLoc =getToLocation()
+            if(!validateInput(toLoc ?:continue)){
+                toLoc = null
+            }
+        }
+        return MoveRequest(parseInput(fromLoc), parseInput(toLoc))
     }
 
-    fun parseInput(fromPos: String, toPos: String): MoveRequest {
+    fun getFromLocation(): String?{
+        println("Choose piece: ")
+        return readLine()
+    }
+
+    fun getToLocation(): String? {
+        println("Choose move: ")
+        return readLine()
+    }
+    fun parseInput(input: String): Coord {
 
         // create the fromPos Coord
-        val fromFile = fromPos[0].toString().toInt()
-        val fromRank = fromPos[1].toString().toInt()
+        val fromFile = input[0].toString().toInt()
+        val fromRank = input[1].toString().toInt()
 
-        val fromCoord = Coord(fromFile, fromRank)
+        return Coord(fromFile, fromRank)
+    }
+    fun validateInput(input: String): Boolean {
+        if(input.length != 2){
+            return false
+        }
+        val inputValues= arrayOf(
+                input[0].toString().toInt(),
+                input[1].toString().toInt())
 
-        // create the toPos Coord
-        val toFile = toPos[0].toString().toInt()
-        val toRank = toPos[1].toString().toInt()
-
-        val toCoord = Coord(toFile, toRank)
-
-        return MoveRequest(fromCoord,toCoord)
-
-
-
-
+        for (value in inputValues) {
+            if( value > 7 || value < 0){
+                return false
+            }
+        }
+        return true
     }
 
 }
