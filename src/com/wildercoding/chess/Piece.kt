d1569141cb40af6ee858a9c887669ab8d92ba0d1
Donovan J. Wilder <donovan.wilder@gmail.com>
2020-12-08 11:09:31 -0500

    added a firstMove Boolean field

--- src/com/wildercoding/chess/Piece.kt
+++ src/com/wildercoding/chess/Piece.kt
@@ -3,6 +3,7 @@ package wildercoding.chess
 import java.text.FieldPosition
 
 abstract class Piece(val type: PieceType, val color: Color) {
+    var firstMove = true
     abstract fun generateMovesList(location: Coord): List<Coord>
     /**
      * Checks to make sure that there is no pieces between the fromPos and the toPos
